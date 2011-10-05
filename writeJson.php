192a53165b0b91501074cd9493ad88904be0819b
Pierrick CAEN <prcaen@gmail.com>
2011-10-05 16:37:19 +0200

    Ajout de la regex

--- writeJson.php
+++ writeJson.php
@@ -1,15 +1,18 @@
 <?php
 
-$geo = $_GET['geo'];
+$geo      = $_GET['geo'];
 $filename = 'gplus.json';
+$pattern  = '[-]?[0-9]*[.]{0,1}[0-9]{4}';
 
-$handleR = fopen($filename, 'r') or die ('can\'t open gplus json');
-$content = fread($handleR, filesize($filename));
-fclose($handleR);
+if(preg_match($pattern,$geo))
+{
+	$handleR = fopen($filename, 'r') or die ('can\'t open gplus json');
+	$content = fread($handleR, filesize($filename));
+	fclose($handleR);
 
-$handleW = fopen($filename, 'w') or die ('can\'t open gplus json');
-$content = str_replace(']', ', ' . $geo . ', 0.5]', $content);
-fwrite($handleW, $content);
-fclose($handleW);
-
+	$handleW = fopen($filename, 'w') or die ('can\'t open gplus json');
+	$content = str_replace(']', ', ' . $geo . ', 0.5]', $content);
+	fwrite($handleW, $content);
+	fclose($handleW);
+}
 ?>
