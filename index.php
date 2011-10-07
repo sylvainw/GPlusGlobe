acffdc2f0a159a9274f71a8b97fe41cb74e60b3d
Pierrick CAEN <prcaen@gmail.com>
2011-10-07 19:41:09 +0200

    Modification des boutons

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $resultPicture    = $mysqli->query($query);
           <a href="https://accounts.google.com/o/oauth2/auth?client_id=926278630057.apps.googleusercontent.com&amp;redirect_uri=http://globeplus.pierrickcaen.fr/oauthcallback.php&amp;scope=https://www.googleapis.com/auth/plus.me&amp;response_type=code" class="button">
           Add my <span>Google +</span> profile</a> on the Globe.
         </strong>
-        <a href="#" class="about">About the project</a>
+        <a href="#" class="button about">About the project</a>
       </h1>
       <div id="wall_picture">
       Just added :
