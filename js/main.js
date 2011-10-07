0d19946bf01335eedd0bbb426d655eeb2c2768d2
Pierrick CAEN <prcaen@gmail.com>
2011-10-07 07:57:05 +0200

    Début d'utilisateur de OAuth

--- js/main.js
+++ js/main.js
@@ -4,31 +4,45 @@ $(document).ready(function()
   var map;
   
   initialize();
-	$('#submit').click(onClickSubmit);
-  $('.credit').click(onClickCredit);
-  $('.credit_close_box').click(onClickCredit);
+  $('#user_add_profil').click(onClickUserAddProfil);
+  $('.about').click(onClickAbout);
+  $('.about_close_box').click(onClickAbout);
 
   function onClickSubmit(e)
   {
-	  e.preventDefault();
-	  $.ajax({
-	    type: 'GET',
-	    url: 'https://www.googleapis.com/plus/v1/people/' + $('#userIdPlus').val(),
-	    data: {'fields' : 'placesLived', 'key' : 'AIzaSyC2b1DFxfc0lNrpmS7fazYKJV0E77ojvpQ'},
-	    success: successPlus,
-	    dataType: 'jsonp'
-	  });
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'https://accounts.google.com/o/oauth2/auth',
+      data: {'client_id' : '699574009061.apps.googleusercontent.com', 
+             'key'       : 'AIzaSyC2b1DFxfc0lNrpmS7fazYKJV0E77ojvpQ',
+             'scope'     : 'https://www.googleapis.com/auth/plus.me',
+             ''},
+      success: successPlus,
+      dataType: 'jsonp'
+    });
   }
-
-  function onClickCredit(e)
+  function onClickSubmit(e)
   {
     e.preventDefault();
-    $('#credit_box').fadeToggle();
+    $.ajax({
+      type: 'GET',
+      url: 'https://www.googleapis.com/plus/v1/people/' + $('#userIdPlus').val(),
+      data: {'fields' : 'placesLived', 'key' : 'AIzaSyC2b1DFxfc0lNrpmS7fazYKJV0E77ojvpQ'},
+      success: successPlus,
+      dataType: 'jsonp'
+    });
+  }
+
+  function onClickAbout(e)
+  {
+    e.preventDefault();
+    $('#about_box').fadeToggle();
   }
 
   function successPlus(data, textStatus)
   {
-	  codeAddress(data.placesLived['0']['value']);
+    codeAddress(data.placesLived['0']['value']);
   }
 
   function initialize() {
