29252d80d76ea752df5c3c932c2c05c125551e8a
Pierrick CAEN <prcaen@gmail.com>
2011-10-08 12:41:22 +0200

    Compression des fichiers js et css

---
+++ js/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('.about').click(onClickAbout);$('.about_close_box').click(onClickAbout);function onClickAbout(e){e.preventDefault();$('#about_box').fadeToggle()}if($('#warn')){$('#warn').fadeOut(14400)}});
